28fd49be0d1cd987c68af58aff94d3a00c1a43e7
Michael Pingel <mdpingel@gmail.com>
2021-02-24 15:47:33 -0800

    working prime number filter
    Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>

--- PrimeSifting/Models/Prime.cs
+++ PrimeSifting/Models/Prime.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace PrimeSifting.Models
@@ -6,13 +7,27 @@ namespace PrimeSifting.Models
   {
     public static List<int> GetPrimes(int userNumber)
     {
-      List<int> Primes = new List<int> { };
+      List<int> intList = new List<int> { };
+      List<int> primeList = new List<int> { };
       for (int i = 2; i <= userNumber; i++)
       {
-        Primes.Add(i);
+        intList.Add(i);
+        primeList.Add(i);
       }
-      return Primes;
+      foreach (int number in intList)
+      {
+        foreach (int otherNumber in intList)
+        {
+          if (number > otherNumber)
+          {
+            if (number % otherNumber == 0)
+            {
+              primeList.Remove(number);
+            }
+          }
+        }
+      }
+      return primeList;
     }
   }
 }
-
